fc7a1f4aafb6fc3ce30d68d9b2b9fdd3b1c1952b
Melanie L. Fryman <melanie@frymanet.com>
2014-11-17 11:26:58 -0600

    initial route setup

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+var port = process.env.PORT;
+var Good = require('good');
 var Hapi = require('hapi');
-var server = new Hapi.Server(3000);
+var server = new Hapi.Server(port);
 
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
 });
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!' + request.query.limit);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/public/{param*}',
+    handler: {
+        directory: {
+            path: 'public'
+        }
+    }
+});
+
+server.pack.register({
+    plugin: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', request: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
+
