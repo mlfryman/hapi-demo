8b33ac0ba715ec4d9aba0c4cd6a779305c35ca64
Melanie L. Fryman <melanie@frymanet.com>
2014-11-17 13:04:51 -0600

    added modules to package.json

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var port = process.env.PORT;
-var Good = require('good');
 var Hapi = require('hapi');
+var Good = require('good');
+var Joi = require('joi');
 var server = new Hapi.Server(port);
 
 server.route({
+    config: {
+        description: 'this is the home page route',
+        notes: 'these are my sweet, sweet notes',
+        tags: ['home', 'routes', 'docs']
+    },
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
@@ -16,6 +22,19 @@ server.route({
     path: '/{name}',
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!' + request.query.limit);
+    },
+    config: {
+        description: 'this is the user name route',
+        notes: 'these are my sweet, sweet notes',
+        tags: ['user', 'routes', 'docs'],
+        validate: {
+            params: {
+                name: Joi.string().min(3).max(7)
+            },
+            query: {
+                limit: Joi.number().required().min(9)
+            }
+        }
     }
 });
 
@@ -29,21 +48,25 @@ server.route({
     }
 });
 
-server.pack.register({
-    plugin: Good,
-    options: {
-        reporters: [{
-            reporter: require('good-console'),
-            args:[{ log: '*', request: '*' }]
-        }]
-    }
-}, function (err) {
-    if (err) {
-        throw err; // something bad happened loading the plugin
-    }
+server.pack.register(
+    [
+        {
+            plugin: Good,
+            options: {
+                reporters: [{
+                    reporter: require('good-console'),
+                    args:[{ log: '*', request: '*' }]
+                }]
+            }
+        },
+        { plugin: require('lout') }
+    ], function (err) {
+        if (err) {
+            throw err; // something bad happened loading the plugin
+        }
 
-    server.start(function () {
-        server.log('info', 'Server running at: ' + server.info.uri);
+        server.start(function () {
+            server.log('info', 'Server running at: ' + server.info.uri);
+        });
     });
-});
 
